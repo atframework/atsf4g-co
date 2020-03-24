3023c1fb575c5b5a8fecb5f42ae9986e3f03673d
owentou <owentou@tencent.com>
2020-03-24 16:51:46 +0800

    Add mbedtls detector and builder

--- project/cmake/ProjectBuildOption.cmake
+++ project/cmake/ProjectBuildOption.cmake
@@ -19,3 +19,15 @@ set(ATFRAME_GATEWAY_MACRO_DATA_SMALL_SIZE 3072 CACHE STRING "small message buffe
 
 option(PROJECT_RESET_DENPEND_REPOSITORIES "Reset depended repositories if it's already exists." OFF)
 option(PROJECT_GIT_CLONE_REMOTE_ORIGIN_DISABLE_SSH "Do not try to use ssh url when clone dependency." OFF)
+
+if (EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    find_package(Git)
+    if(NOT EXISTS ${ATFRAMEWORK_ATFRAME_UTILS_REPO_DIR})
+        execute_process(COMMAND ${GIT_EXECUTABLE} config "remote.origin.url"
+            OUTPUT_VARIABLE PROJECT_GIT_REMOTE_ORIGIN_URL
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        string(REGEX MATCH "^http(s?):" PROJECT_GIT_REMOTE_ORIGIN_USE_SSH "${PROJECT_GIT_REMOTE_ORIGIN_URL}")
+    endif()
+endif()
