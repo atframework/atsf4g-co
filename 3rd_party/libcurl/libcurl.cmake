c2e755978b74f312c9aa66348141a657497984fd
owentou <owentou@tencent.com>
2020-05-28 19:34:40 +0800

    增加fmtlib，修订libcurl的构建流程，优先使用cmake module

--- 3rd_party/libcurl/libcurl.cmake
+++ 3rd_party/libcurl/libcurl.cmake
@@ -23,8 +23,8 @@ macro(PROJECT_3RD_PARTY_LIBCURL_IMPORT)
                 list(APPEND 3RD_PARTY_PUBLIC_INCLUDE_DIRS ${CURL_INCLUDE_DIRS})
             endif ()
 
-            if(CURL_LIBRARIES)
-                list(APPEND 3RD_PARTY_PUBLIC_LINK_NAMES ${CURL_LIBRARIES})
+            if(3RD_PARTY_LIBCURL_STATIC_LINK_NAMES)
+                list(APPEND 3RD_PARTY_PUBLIC_LINK_NAMES ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES})
             endif()
         endif ()
     endif()
@@ -36,8 +36,15 @@ if (VCPKG_TOOLCHAIN)
 endif ()
 
 if (NOT CURL_FOUND)
-    if (Libcurl_ROOT)
+    if (CURL_ROOT)
+        set(Libcurl_ROOT ${CURL_ROOT})
+        set(LIBCURL_ROOT ${CURL_ROOT})
+    elseif (Libcurl_ROOT)
         set(LIBCURL_ROOT ${Libcurl_ROOT})
+        set(CURL_ROOT ${Libcurl_ROOT})
+    elseif (LIBCURL_ROOT)
+        set(Libcurl_ROOT ${LIBCURL_ROOT})
+        set(CURL_ROOT ${LIBCURL_ROOT})
     endif()
 
     if (LIBCURL_ROOT)
@@ -58,7 +65,7 @@ if (NOT CURL_FOUND)
     FindConfigurePackage(
         PACKAGE CURL
         BUILD_WITH_CMAKE
-        CMAKE_FLAGS "-DCURL_STATICLIB=NO"
+        CMAKE_FLAGS "-DBUILD_SHARED_LIBS=NO"
         WORKING_DIRECTORY "${3RD_PARTY_LIBCURL_PKG_DIR}"
         BUILD_DIRECTORY "${3RD_PARTY_LIBCURL_PKG_DIR}/${3RD_PARTY_LIBCURL_PKG_NAME}/build_jobs_${PROJECT_PREBUILT_PLATFORM_NAME}"
         PREFIX_DIRECTORY "${3RD_PARTY_LIBCURL_ROOT_DIR}"
@@ -74,97 +81,98 @@ if (NOT CURL_FOUND)
     endif()
 
     unset(3RD_PARTY_LIBCURL_STATIC_LINK_NAMES)
-
-    set(3RD_PARTY_LIBCURL_TEST_SRC "#include <curl/curl.h>
-    #include <stdio.h>
-
-    int main () {
-        curl_global_init(CURL_GLOBAL_ALL)\;
-        printf(\"libcurl version: %s\", LIBCURL_VERSION)\;
-        return 0\; 
-    }")
-
-    file(WRITE "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c" ${3RD_PARTY_LIBCURL_TEST_SRC})
-
-    if(MSVC)
-        try_compile(3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
-            ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
-            CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CURL_INCLUDE_DIRS}
-            LINK_LIBRARIES ${CURL_LIBRARIES}
-            OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG
-        )
+    if (TARGET CURL::libcurl)
+        EchoWithColor(COLOR GREEN "-- Libcurl: use target CURL::libcurl")
     else()
-        try_run(3RD_PARTY_LIBCURL_TRY_RUN_RESULT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
-            ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
-            CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CURL_INCLUDE_DIRS}
-            LINK_LIBRARIES ${CURL_LIBRARIES}
-            COMPILE_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG
-            RUN_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_RUN_OUT
-        )
-    endif()
+        set(3RD_PARTY_LIBCURL_TEST_SRC "#include <curl/curl.h>
+        #include <stdio.h>
 
-    if (NOT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT)
-        EchoWithColor(COLOR YELLOW "-- Libcurl: Dynamic symbol test in ${CURL_LIBRARIES} failed, try static symbols")
+        int main () {
+            curl_global_init(CURL_GLOBAL_ALL)\;
+            printf(\"libcurl version: %s\", LIBCURL_VERSION)\;
+            return 0\; 
+        }")
+
+        file(WRITE "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c" ${3RD_PARTY_LIBCURL_TEST_SRC})
 
         if(MSVC)
-            if (ZLIB_FOUND)
-                list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${ZLIB_LIBRARIES})
-            endif()
-
             try_compile(3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
                 ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
                 CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CURL_INCLUDE_DIRS}
-                COMPILE_DEFINITIONS /D CURL_STATICLIB
-                LINK_LIBRARIES ${CURL_LIBRARIES} ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES}
-                OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG
+                LINK_LIBRARIES ${CURL_LIBRARIES}
+                OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG
             )
         else()
-            get_filename_component(3RD_PARTY_LIBCURL_LIBDIR ${CURL_LIBRARIES} DIRECTORY)
-            find_package(PkgConfig)
-            if (PKG_CONFIG_FOUND AND EXISTS "${3RD_PARTY_LIBCURL_LIBDIR}/pkgconfig/libcurl.pc")
-                pkg_check_modules(LIBCURL "${3RD_PARTY_LIBCURL_LIBDIR}/pkgconfig/libcurl.pc")
-                list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${LIBCURL_STATIC_LIBRARIES})
-                list(REMOVE_ITEM 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES curl)
-                message(STATUS "Libcurl use static link with ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES} in ${3RD_PARTY_LIBCURL_LIBDIR}")
-            else()
-                if (OPENSSL_FOUND)
-                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${OPENSSL_LIBRARIES})
-                else ()
-                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ssl crypto)
-                endif()
-                if (ZLIB_FOUND)
-                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${ZLIB_LIBRARIES})
-                else ()
-                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES z)
-                endif()
-            endif ()
-            
             try_run(3RD_PARTY_LIBCURL_TRY_RUN_RESULT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
                 ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
-                CMAKE_FLAGS -DCMAKE_INCLUDE_DIRECTORIES_BEFORE=${CURL_INCLUDE_DIRS}
-                COMPILE_DEFINITIONS -DCURL_STATICLIB
-                LINK_LIBRARIES ${CURL_LIBRARIES} ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES} -lpthread
-                COMPILE_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG
+                CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CURL_INCLUDE_DIRS}
+                LINK_LIBRARIES ${CURL_LIBRARIES}
+                COMPILE_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG
                 RUN_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_RUN_OUT
             )
-            list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES pthread)
         endif()
 
         if (NOT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT)
-            message(STATUS ${3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG})
-            message(STATUS ${3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG})
-            message(FATAL_ERROR "Libcurl: try compile with ${CURL_LIBRARIES} failed")
+            EchoWithColor(COLOR YELLOW "-- Libcurl: Dynamic symbol test in ${CURL_LIBRARIES} failed, try static symbols")
+            if(MSVC)
+                if (ZLIB_FOUND)
+                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${ZLIB_LIBRARIES})
+                endif()
+
+                try_compile(3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
+                    ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
+                    CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CURL_INCLUDE_DIRS}
+                    COMPILE_DEFINITIONS /D CURL_STATICLIB
+                    LINK_LIBRARIES ${CURL_LIBRARIES} ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES}
+                    OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG
+                )
+            else()
+                get_filename_component(3RD_PARTY_LIBCURL_LIBDIR ${CURL_LIBRARIES} DIRECTORY)
+                find_package(PkgConfig)
+                if (PKG_CONFIG_FOUND AND EXISTS "${3RD_PARTY_LIBCURL_LIBDIR}/pkgconfig/libcurl.pc")
+                    pkg_check_modules(LIBCURL "${3RD_PARTY_LIBCURL_LIBDIR}/pkgconfig/libcurl.pc")
+                    list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${LIBCURL_STATIC_LIBRARIES})
+                    list(REMOVE_ITEM 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES curl)
+                    message(STATUS "Libcurl use static link with ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES} in ${3RD_PARTY_LIBCURL_LIBDIR}")
+                else()
+                    if (OPENSSL_FOUND)
+                        list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${OPENSSL_LIBRARIES})
+                    else ()
+                        list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ssl crypto)
+                    endif()
+                    if (ZLIB_FOUND)
+                        list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES ${ZLIB_LIBRARIES})
+                    else ()
+                        list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES z)
+                    endif()
+                endif ()
+                
+                try_run(3RD_PARTY_LIBCURL_TRY_RUN_RESULT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT
+                    ${CMAKE_BINARY_DIR} "${CMAKE_BINARY_DIR}/try_run_libcurl_test.c"
+                    CMAKE_FLAGS -DCMAKE_INCLUDE_DIRECTORIES_BEFORE=${CURL_INCLUDE_DIRS}
+                    COMPILE_DEFINITIONS -DCURL_STATICLIB
+                    LINK_LIBRARIES ${CURL_LIBRARIES} ${3RD_PARTY_LIBCURL_STATIC_LINK_NAMES} -lpthread
+                    COMPILE_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG
+                    RUN_OUTPUT_VARIABLE 3RD_PARTY_LIBCURL_TRY_RUN_OUT
+                )
+                list(APPEND 3RD_PARTY_LIBCURL_STATIC_LINK_NAMES pthread)
+            endif()
+            if (NOT 3RD_PARTY_LIBCURL_TRY_COMPILE_RESULT)
+                message(STATUS ${3RD_PARTY_LIBCURL_TRY_COMPILE_DYN_MSG})
+                message(STATUS ${3RD_PARTY_LIBCURL_TRY_COMPILE_STA_MSG})
+                message(FATAL_ERROR "Libcurl: try compile with ${CURL_LIBRARIES} failed")
+            else()
+                EchoWithColor(COLOR GREEN "-- Libcurl: use static symbols")
+                if(3RD_PARTY_LIBCURL_TRY_RUN_OUT)
+                    message(STATUS ${3RD_PARTY_LIBCURL_TRY_RUN_OUT})
+                endif()
+                list(APPEND 3RD_PARTY_PUBLIC_DEFINITIONS CURL_STATICLIB=1)
+            endif()
         else()
-            EchoWithColor(COLOR GREEN "-- Libcurl: use static symbols")
+            EchoWithColor(COLOR GREEN "-- Libcurl: use dynamic symbols")
             if(3RD_PARTY_LIBCURL_TRY_RUN_OUT)
                 message(STATUS ${3RD_PARTY_LIBCURL_TRY_RUN_OUT})
             endif()
-            list(APPEND 3RD_PARTY_PUBLIC_DEFINITIONS CURL_STATICLIB=1)
-        endif()
-    else()
-        EchoWithColor(COLOR GREEN "-- Libcurl: use dynamic symbols")
-        if(3RD_PARTY_LIBCURL_TRY_RUN_OUT)
-            message(STATUS ${3RD_PARTY_LIBCURL_TRY_RUN_OUT})
         endif()
     endif()
     unset(3RD_PARTY_LIBCURL_TRY_RUN_OUT)
