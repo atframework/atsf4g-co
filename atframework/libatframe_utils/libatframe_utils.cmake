78bf7772dc8200b3d6bdd756474615750b8a4157
owent <owt5008137@live.com>
2020-10-01 09:34:44 +0800

    优化atframework导入

--- atframework/libatframe_utils/libatframe_utils.cmake
+++ atframework/libatframe_utils/libatframe_utils.cmake
@@ -1,11 +1,2 @@
 ﻿
 # =========== libatframe_utils ==================
-
-if(TARGET ${3RD_PARTY_ATFRAME_UTILS_LINK_NAME})
-    message(STATUS "${3RD_PARTY_ATFRAME_UTILS_LINK_NAME} already exist, use it for ${PROJECT_NAME} directly.")
-else()
-    if (NOT EXISTS "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
-        file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
-    endif ()
-    add_subdirectory(${3RD_PARTY_ATFRAME_UTILS_PKG_DIR} "${CMAKE_BINARY_DIR}/deps/${3RD_PARTY_ATFRAME_UTILS_LINK_NAME}")
-endif()
