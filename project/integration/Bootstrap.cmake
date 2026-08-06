c16ac819c4447725dd094698354f5ee34e4c8a9d
owent <admin@owent.net>
2026-08-06 21:17:10 +0800

    集成优化

--- project/integration/Bootstrap.cmake
+++ project/integration/Bootstrap.cmake
@@ -1,6 +1,6 @@
 include_guard(GLOBAL)
 
-if(EXISTS "${PROJECT_SOURCE}/.vscode")
+if(EXISTS "${PROJECT_SOURCE_DIR}/.vscode")
   message(STATUS "Install VSCode Integration.")
   include("vscode/OptimizeVsCodeSettings.cmake")
 endif()
