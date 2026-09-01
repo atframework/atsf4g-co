fa0215eac84cb2a2772af2c454a11073c6dd321e
yousongyang <yousongyang0105@gmail.com>
2026-09-01 11:01:59 +0800

    删除无用代码

--- src/lobbysvr/service/data/user.h
+++ src/lobbysvr/service/data/user.h
@@ -101,9 +101,6 @@ class user : public user_cache {
     time_t refresh_feature_limit_second;
     time_t refresh_feature_limit_minute;
     time_t refresh_feature_limit_hour;
-    // PROJECT_NAMESPACE_ID::SCUserLevelupSyn user_level_up_syn;
-
-    atfw::memory::stl::unordered_map<int32_t, PROJECT_NAMESPACE_ID::DItemInstance> dirty_item_by_type;
 
     atfw::memory::stl::unordered_map<uintptr_t, dirty_sync_handle_t> dirty_handles;
     gsl::string_view current_dirty_handle_name;
